543e8abd5e10e5d9488fc6e6c62b794a0cdf6028
21810018 - Le Minh Khoi <khoi.leminh.stds@gmail.com>
2024-11-13 09:50:15 +0700

    Reupload

--- index.js
+++ index.js
@@ -3,9 +3,35 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 80;
+const expressHandlebars = require('express-handlebars');
+
 
 //Cấu hình web : public folder
 app.use(express.static(__dirname+'/public'));
+
+//Cấu hình express-handlebars
+app.engine('hbs', expressHandlebars.engine({
+    layoutsDir: __dirname + '/views/layouts',
+    partialsDir: __dirname + '/views/partials',
+    extname: 'hbs',
+    defaultLayout: 'layout'
+}));
+app.set('view engine','hbs');
+
+//Cấu hình routes
+app.get('/createTAbles', (req,res) => {
+    let models = require('/models');
+    models.sequelize.sync().then(() => {
+        res.send('Table created !')
+    });
+});
+app.get('/', (req,res) => {
+    res.render('index');
+});
+
+app.get('/:page',(req,res)=>{
+    res.render(req.params.page);
+});
 
 //Khởi động web server
 app.listen(port, () => {
