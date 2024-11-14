d4fb23e464edd7ba85981cd74a0098b08f1ee8f4
21810018 - Le Minh Khoi <khoi.leminh.stds@gmail.com>
2024-11-14 17:02:59 +0700

    Product Tags

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -3,10 +3,48 @@ const models = require('../models');
 
 
 controller.show = async (req,res) => {
-    let products = await models.Product.findAll({
+    let category = isNaN(req.query.category) ? 0 : parseInt(req.query.category);
+    let brand = isNaN(req.query.brand)? 0 : parseInt(req.query.brand);
+    let tag = isNaN(req.query.tag)? 0 : parseInt(req.query.tag);
+    //Product list
+    let options = {
         atrtibutes:['id','name','imagePath','stars','price','oldPrice'],
+        where: {}
+    };
+
+    if (category > 0){
+        options.where.categoryId = category;
+    }
+
+    if (brand > 0){
+        options.where.brandId = brand;
+    }
+
+    if (tag > 0){
+        options.where.brandId = tag;
+    }
+
+    let products = await models.Product.findAll(options);
+
+    // Category List
+    let categories = await models.Category.findAll({
+        include:[{
+            model: models.Product
+        }],
+        order:['name']
     });
-    res.render('product-list',{products});
+
+    // Brand List
+    let brands = await models.Brand.findAll({
+        include: [{
+           model: models.Product
+        }]
+    });
+
+    // Tag list
+    let tags = await models.Tag.findAll();
+
+    res.render('product-list',{products,categories,brands,tags});
 }
 
 module.exports = controller;
